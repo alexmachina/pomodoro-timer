e36db81031782f057235d3a4da506c60e533210f
Alex Alonso <alex.alonso-ext@viavarejo.com.br>
2020-09-29 19:12:32 -0300

    Adiciona um teste

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,16 +5,19 @@ module.exports = {
     project: "./tsconfig.json",
     ecmaFeatures: { jsx: true },
   },
+  plugins: ["jest"],
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:jest/recommended",
   ],
   env: {
     browser: true,
     node: true,
     es6: true,
+    "jest/globals": true,
   },
 
   settings: {
---
+++ src/components/PlayButton/index.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import PlayButton from "./index";
+
+test("PlayButton", () => {
+  const onClick = jest.fn();
+  const { container } = render(<PlayButton onClick={onClick} />);
+  expect(container).toMatchSnapshot();
+});
